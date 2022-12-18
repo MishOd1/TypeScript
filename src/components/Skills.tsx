31fe3be37f05be2b7682c4a88dc8bb8b0002a1ec
MishOd1 <nbay6i@hotmail.com>
2022-12-18 20:37:57 +0300

    #final

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -18,17 +18,19 @@ const Skills = () => {
     const Tailwind = require("../assets/tailwind.png")
     const Python = require("../assets/Python69.png")
     const Pine = require("../assets/pine.png")
+    const Play = require("../assets/playw.png")
+    const Vite = require("../assets/vite.png")
   return (
     <div id='skills111' className='w-full h-screen bg-[#15243f] text-gray-300'>
       {/* Container */}
 
-      <div className='max-w-[75%] ml-[12.5%] mx-auto p-4 flex text-center flex-col justify-center w-full h-full'>
+      <div className='max-w-[65%] ml-[17.5%] mx-auto p-4 flex text-center flex-col justify-center w-full h-full'>
           <div>
               <p  className='text-4xl text-center font-bold inline border-b-4 border-pink-600 '>Skills</p>
               <p className='py-4 text-center'>// These are the technologies I've worked with </p>
           </div>
 
-          <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
+          <div className='w-full grid grid-cols-2 sm:grid-cols-5 gap-4 text-center py-8'>
               <div className='shadow-md bg-[#0a192f] shadow-[#040c16] hover:scale-110 duration-500 hover:shadow-[#53d8e96c]'>
                   <img className='w-[40%] mx-auto' src={HTML} alt="HTML icon" />
                   <p className='my-4'>HTML</p>
@@ -62,6 +64,14 @@ const Skills = () => {
               <div className='shadow-md bg-[#0a192f] shadow-[#040c16] hover:scale-110 duration-500  hover:shadow-[#53d8e96c]'>
                   <img className='w-[50%] mx-auto' src={Pine} alt="HTML icon" />
                   <p className='my-6'>PineScript</p>
+              </div>
+              <div className='shadow-md bg-[#0a192f] shadow-[#040c16] hover:scale-110 duration-500  hover:shadow-[#53d8e96c]'>
+                  <img className='w-[50%] mx-auto' src={Play} alt="HTML icon" />
+                  <p className='my-6'>PlayWright.JS</p>
+              </div>
+              <div className='shadow-md bg-[#0a192f] shadow-[#040c16] hover:scale-110 duration-500  hover:shadow-[#53d8e96c]'>
+                  <img className='w-[40%] mx-auto' src={Vite} alt="HTML icon" />
+                  <p className='my-4'>Vite.JS</p>
               </div>
           </div>
       </div>
